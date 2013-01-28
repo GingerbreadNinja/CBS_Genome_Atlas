28db36f97cdaecb588c29779880903dd2560c754
Helen Cook <helen.cook@gmail.com>
2013-01-28 15:12:46 +0100

    move make tag into submit_jobs_to_queue.py, and appropriate changes in Makefile so that you can specify what to run

--- analysis/submit_jobs_to_queue.py
+++ analysis/submit_jobs_to_queue.py
@@ -25,7 +25,7 @@ from subprocess import call
 debug = True
 
 def usage():
-    sys.stderr.write('usage:\n\t' + sys.argv[0] + ' --accession=CP000020 --version=2\n\t' + sys.argv[0] + ' --av=CP000020_2\n\t' + sys.argv[0] + ' --all [--backfill]\n');
+    sys.stderr.write('usage:\n\t' + sys.argv[0] + ' --accession=CP000020 --version=2\n\t' + sys.argv[0] + ' --av=CP000020_2\n\t' + sys.argv[0] + ' --latest\t(run on everything since script last ran)\n\t' + sys.argv[0] + ' --backfill\t(run on everything)\noptional: --maketag=populate_tax\n');
     sys.exit(2)
 
 def db_connect():
@@ -34,23 +34,30 @@ def db_connect():
     cur = db.cursor()
     return cur 
 
-def register_job(accession, version):
-    job_id = 1 #TODO do not hardcode this
+def register_job(accession, version, maketag):
+    job_id = 0
+    if maketag == "single_genome":
+        job_id = 1 
+    if maketag == "populate_tax":
+        job_id = 2
+    if job_id == 0:
+        sys.stderr.write("maketag " + maketag + " is invalid.\n")
+        usage()
     job_uuid = uuid.uuid1()
     cur=db_connect()
     cur.execute("""INSERT INTO active_job (job_id, submission_time, job_uuid, accession, version, status) VALUES (%s, now(), %s, %s, %s, 'In Progress')""", (job_id, job_uuid, accession, version))
     return str(job_uuid)
 
-def process_one_genome(accession, version):
+def process_one_genome(accession, version, maketag):
     # TODO check that accession file exists on disk
     # TODO if not passed version, get latest version for this accession number
 
     # write to db active_jobs table that we are submitting to the queueing system
-    job_uuid = register_job(accession, version)
+    job_uuid = register_job(accession, version, maketag)
 
     # TODO get this information from a config file
     logging_dir = "/home/panfs/cbs/projects/cge/data/public/genome_sync/log/"
-    call(["xmsub", "-l", "mem=4gb,walltime=3600,procs=8,partition=cge-cluster", "-de", "-ro", logging_dir + accession + "_" + job_uuid + ".log", "-re", logging_dir + accession + "_" + job_uuid + ".out", "-N", accession + "_" + str(version) + "_" + job_uuid, "-r", "y", "make -i -k", "ACCESSION=" + accession, "VERSION=" + str(version), "JOB_UUID=" + job_uuid])
+    call(["xmsub", "-l", "mem=4gb,walltime=3600,procs=8,partition=cge-cluster", "-de", "-ro", logging_dir + accession + "_" + job_uuid + ".log", "-re", logging_dir + accession + "_" + job_uuid + ".out", "-N", accession + "_" + str(version) + "_" + job_uuid, "-r", "y", "make -i -k", maketag, "ACCESSION=" + accession, "VERSION=" + str(version), "JOB_UUID=" + job_uuid])
  
 def get_last_runtime(backfill):
     # check in db for latest runtime
@@ -77,25 +84,25 @@ def new_genomes(time):
     cur.execute("""SELECT accession, version FROM bioproject, genome, replicon WHERE genome.bioproject_id = bioproject.bioproject_id AND genome.genome_id = replicon.genome_id AND modify_date >= %s""", (time)) 
     return cur.fetchall()
 
-def process_new_genomes(backfill):
+def process_new_genomes(backfill, maketag):
     last_runtime = get_last_runtime(backfill)
     log_new_run()
     for accession, version in new_genomes(last_runtime):
         if debug:
             print "accession = " + accession + "\tversion = " + str(version)
-        process_one_genome(accession, version)
+        process_one_genome(accession, version, maketag)
     
 def failed_genomes():
     # get list of all genomes to reprocess;
     return []
 
-def process_failed_genomes():
+def process_failed_genomes(maketag):
     for accession in failed_genomes():
-        process_one_genome(accession)
+        process_one_genome(accession, version, maketag)
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "all", "accession=", "version=", "av=", "backfill"])
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "latest", "accession=", "version=", "av=", "backfill", "maketag="])
     except getopt.GetoptError as err:
         sys.stderr.write(str(err) + "\n")
         usage()
@@ -103,10 +110,11 @@ def main(argv):
     accession = ""
     version = ""
     backfill = False
+    maketag = "single_genome"
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
-        elif o in ("--all"):
+        elif o in ("--latest"):
             plan = "all"
         elif o in ("--accession"):
             plan = "one"
@@ -115,21 +123,24 @@ def main(argv):
             version = a
         elif o in ("--av"):
             plan = "one"
-            accession=a[0:8]
+            accession=a[0:8] #XXX this breaks for badly formatted accession numbers
             version=a[-1]
         elif o in ("--backfill"):
+            plan = "all"
             backfill = True
+        elif o in ("--maketag"):
+            maketag = a
         else:
             sys.stderr.write("Unregognized option " + o + "\n");
             usage()
     if plan == "all":
-        process_new_genomes(backfill)
-        process_failed_genomes()
+        process_new_genomes(backfill, maketag)
+        #process_failed_genomes(maketag) TODO
     elif plan == "one":
         if version == "" or accession == "":
             usage()
         else:
-            process_one_genome(accession, version)
+            process_one_genome(accession, version, maketag)
     else:
         usage()
 
